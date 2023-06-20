a53e699e044832ac240f81c436b0bdb27eda646f
Jack Sebben <jacksebben11@hotmail.com>
2023-06-20 07:52:19 -0400

    The game logic to check for matches and tell the player how many of the guesses were 'exact matches' and how many of the guesses were present in the code.
    To-Do:
    
    Display the current board with the user's guesses and 'exact matches'/'present numbers'
    
    Create a round loop that will exit the loop if the player wins and annouce win, or will announce computer as winner if round 12 is finished with no exact matches == 4

--- game.rb
+++ game.rb
@@ -1,27 +1,33 @@
+require 'pry-byebug'
 module GameLogic
     def self.match?(player_guess, code)
         comparison = []
         code.each {|num| comparison.push(num)}
+        guess = []
+        player_guess.each {|num| guess.push(num)}
 
         positions_correct = 0
         numbers_correct = 0
 
         # Iterate through ea. number of the player's guess
-        player_guess.each do |number|
+        guess.each do |number|
         # if player's number matches any numbers in code ...
-            if comparison.include?(number) 
-                if player_guess.index(number) == comparison.index(number)
+            if comparison.include?(number)
+                number_index = guess.index(number) 
+                if number == comparison[number_index]
                     positions_correct += 1
-                    comparison[comparison.index(number)] = 'X-act'
+                    comparison[number_index] = 'X-act'
+                    guess[number_index] = 'X-act'
                 elsif 
                     numbers_correct += 1
                     comparison[comparison.index(number)] = 'present'
+                    guess[number_index] = 'present'
                 end 
             end 
         end  
-        p comparison
-        puts "Exact Matches: #{positions_correct}"
-        puts "Correct Numbers: #{numbers_correct}"
+        return positions_correct, numbers_correct
     end 
+
+
 
 end
--- mastermind.rb
+++ mastermind.rb
@@ -4,28 +4,29 @@ require_relative 'player'
 require_relative 'game'
 
 board = Board.new()
-# Computer sets a code
 code = ComputerSetter.set_code
 round = 1
 rounds_left = 12 - (round - 1)
-p code
+code = [4,4,2,2]
 
 # Round loop:
 # until round == 13 do ...
     # player is introduced to guess the code:
     puts "You have #{rounds_left} attempts to guess the 4 digit code (between 1 and 6)"
-    #
 
     # Player enters 4 digits between 1 - 6
     players_guess = PlayerSetter.make_guess
-        # Game checks if digits are valid: 
-        # Entry is only numbers
-        # Each digit is 1 - 6
-        # 'else' it will ask for the player's input again
-    #
-    # Game checks to see if player's input matches the computer's 4 digit code
-    GameLogic.match?(players_guess, code)
 
+    # Game checks to see if player's input matches the computer's 4 digit code
+    positions_correct, numbers_correct = GameLogic.match?(players_guess, code)
+    if positions_correct == 4
+        puts "You broke the code! You won the game and beat the computer!"
+    else 
+        puts "Numbers in correct position/order: #{positions_correct}"
+        puts "Numbers in the code, but not in the correct position/order: #{numbers_correct}"
+    end 
+    p positions_correct
+    p numbers_correct
     p players_guess
     p code
 
