559c0eae0ab99e2e8e63cd98050f820e04957c1f
Jack Sebben <jacksebben11@hotmail.com>
2023-06-20 08:21:07 -0400

    Now the game will 'physically' changed the board - specifically, the 'color_rows' and 'peg_rows'. Displaying the user's past gueses in the color_rows and the exact/present matches. This will allow us to display the board to the user/player so they can make future decisions and track guesses.

--- mastermind.rb
+++ mastermind.rb
@@ -5,14 +5,11 @@ require_relative 'game'
 
 board = Board.new()
 code = ComputerSetter.set_code
-round = 1
-rounds_left = 12 - (round - 1)
-code = [4,4,2,2]
+code = [4,3,2,2]
 
-# Round loop:
-# until round == 13 do ...
+board.color_rows.each_index do |current_row| # Each round == each row on board
     # player is introduced to guess the code:
-    puts "You have #{rounds_left} attempts to guess the 4 digit code (between 1 and 6)"
+    puts "You have #{12 - current_row} attempts to guess the 4 digit code (between 1 and 6)"
 
     # Player enters 4 digits between 1 - 6
     players_guess = PlayerSetter.make_guess
@@ -25,10 +22,8 @@ code = [4,4,2,2]
         puts "Numbers in correct position/order: #{positions_correct}"
         puts "Numbers in the code, but not in the correct position/order: #{numbers_correct}"
     end 
-    p positions_correct
-    p numbers_correct
-    p players_guess
-    p code
+    board.color_rows[current_row] = players_guess
+    board.peg_rows[current_row][0] = "Exact = #{positions_correct}"
+    board.peg_rows[current_row][1] = "Present = #{numbers_correct}"
 
-    round += 1
-# end "round loop"
+end 
